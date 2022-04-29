aee92b76c3016fe89135b7e8f5ac1f8297cd6722
sofar <mahmoud.abohawis10@gmail.com>
2022-04-29 16:56:03 +0200

    modified main

--- 1_Misc/3_prj-LinkedList/0_Code/3_App/main.c
+++ 1_Misc/3_prj-LinkedList/0_Code/3_App/main.c
@@ -2,8 +2,16 @@
 int main(){
  struct LinkedList *list = (struct LinkedList*)malloc(sizeof(struct LinkedList));
  push(5,list);
+ push(6,list);
+ push(9,list);
  reverse(list);
  display(list);
-  
+ push(10,list);
+  printf("\n"); 
+  reverse(list);
+  display(list);
+  Delete(5,list);
+  printf("\n");
+  display(list);
     return 0;
 }
