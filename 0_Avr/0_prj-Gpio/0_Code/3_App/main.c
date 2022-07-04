245ef1243a5a7c26325ee10e59a678361ee75643
sofar <mahmoud.abohawis10@gmail.com>
2022-07-04 20:30:54 +0200

    Naming Variable

--- 0_Avr/0_prj-Gpio/0_Code/3_App/main.c
+++ 0_Avr/0_prj-Gpio/0_Code/3_App/main.c
@@ -11,19 +11,19 @@ int main()
     /* This program should blink pin 13 of Arduino Uno  when pressing button */
 
     /* Initialization */
-    Pin_mode(GPIO_B, LED_PIN, Output);
-    Pin_mode(GPIO_C, BUTTON_PIN, Input);
+    PinMode(GPIO_B, LED_PIN, Output);
+    PinMode(GPIO_C, BUTTON_PIN, Input);
 
     /* Program Loop */
     while (1)
     {
-        if(pinStaus(GPIO_C, BUTTON_PIN))
+        if(PinStaus(GPIO_C, BUTTON_PIN))
         {
-            digital_write(GPIO_B, LED_PIN, High);
+            DigitalWrite(GPIO_B, LED_PIN, High);
         }
         else
         {
-            digital_write(GPIO_B, LED_PIN, Low);
+            DigitalWrite(GPIO_B, LED_PIN, Low);
         }
     }
 
